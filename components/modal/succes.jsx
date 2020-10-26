f0abaa8036e6f2818420f39d4508a59150e6eccc
amardito <amardito37@gmail.com>
2020-10-26 20:53:20 +0700

    adding success modal

---
+++ components/modal/succes.jsx
@@ -0,0 +1,12 @@
+import s from '../../styles/components/succes.module.css'
+
+const Success = () => {
+  return(
+    <div className={s.outer}>
+      <div className={s.card}>
+        
+      </div>
+    </div>
+  )
+}
+export default Success
