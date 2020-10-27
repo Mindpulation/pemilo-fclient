edfdd3e7f50619e2310a989badcb1fb454b10ca4
Reza Andriansyah <technoboth@gmail.com>
2020-10-28 04:30:36 +0700

    create modal success state plot

--- global/context/val.js
+++ global/context/val.js
@@ -3,4 +3,4 @@ import React from 'react';
 export const MODALCONTEXT1 = {
   STATEMODALCONTEXT1    : React.createContext(),
   DISPATCHMODALCONTEXT1 : React.createContext()
-}
+};
--- pages/page/[room]/confirm/index.jsx
+++ pages/page/[room]/confirm/index.jsx
@@ -1,6 +1,28 @@
+import { useContext } from 'react';
+import { MODALCONTEXT1 } from '../../../../global/context/val';
+
+import { actionModal } from '../../../../global/actions/index';
+
+import ModalSuccess from '../../../../components/modal/success.jsx';
+
+const OBJ_MODAL1 = actionModal("Modal1");
+
+const { STATEMODALCONTEXT1, DISPATCHMODALCONTEXT1 } = MODALCONTEXT1;
+
 const Confirm = () => {
+
+  const modalState1 = useContext(STATEMODALCONTEXT1);
+  const modalDispatch1 = useContext(DISPATCHMODALCONTEXT1);
+
+  const atShowModal = () => {
+    modalDispatch1( {tipe : OBJ_MODAL1.SHOW_MODAL } );
+  }
+
   return(
-    <div></div>
+    <div>
+      <ModalSuccess></ModalSuccess>
+      <button onClick={atShowModal} >Show Modal</button>
+    </div>
   )
 }
 
