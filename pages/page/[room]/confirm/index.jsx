d2adabd5a0a9df3567b896bb24eb2083403ac22e
amardito <amardito37@gmail.com>
2020-10-28 20:16:41 +0700

    adding React.memo on page & component

--- pages/page/[room]/confirm/index.jsx
+++ pages/page/[room]/confirm/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import React from 'react'
 import { MODALCONTEXT1 } from '../../../../global/context/val';
 import { actionModal } from '../../../../global/actions/index';
 import ModalSuccess from '../../../../components/modal/success.jsx';
@@ -7,20 +8,22 @@ const OBJ_MODAL1 = actionModal("Modal1");
 
 const { STATEMODALCONTEXT1, DISPATCHMODALCONTEXT1 } = MODALCONTEXT1;
 
-const Confirm = () => {
-
-  const modalDispatch1 = useContext(DISPATCHMODALCONTEXT1);
-
-  const atShowModal = () => {
-    modalDispatch1( {tipe : OBJ_MODAL1.SHOW_MODAL } )
+const Confirm = React.memo(
+  () => {
+  
+    const modalDispatch1 = useContext(DISPATCHMODALCONTEXT1);
+  
+    const atShowModal = () => {
+      modalDispatch1( {tipe : OBJ_MODAL1.SHOW_MODAL } )
+    }
+  
+    return(
+      <div>
+        <ModalSuccess></ModalSuccess>
+        <button onClick={atShowModal} >Show Modal</button>
+      </div>
+    )
   }
-
-  return(
-    <div>
-      <ModalSuccess></ModalSuccess>
-      <button onClick={atShowModal} >Show Modal</button>
-    </div>
-  )
-}
+)
 
 export default Confirm;
