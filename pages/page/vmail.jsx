f2deb08aa5cbbe67a00cb6df447310eeb73ea6af
Reza Andriansyah <technoboth@gmail.com>
2020-11-22 17:05:35 +0700

    add handler error 90%

--- pages/page/vmail.jsx
+++ pages/page/vmail.jsx
@@ -4,24 +4,9 @@ import Mobile from '../../layout/mobile'
 import React, { useEffect, useState } from 'react'
 import ImageBlur from '../../components/image/blur'
 
-import { set, get, del } from '../../hooks/localStorage';
-import { findAnggota, findRoom, checkRoomPass } from '../../api/index';
+import { set, get } from '../../hooks/localStorage';
+import { findAnggota, checkRoomPass } from '../../api/index';
 import { useRouter } from 'next/router';
-
-const VmailNoValid = () => {
-  return(
-    <React.Fragment>
-      <Head>
-        <title>Page Email Validate</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Mobile>
-        <span>404</span>
-      </Mobile>
-    </React.Fragment>
-  );
-}
 
 const Vmail = () => {  
 
@@ -71,51 +56,44 @@ const Vmail = () => {
     }
 
   }    
+  
+  return(
+    <React.Fragment>
+      
+      <Head>
+        <title>Page Email Validate</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
 
-  if(room === undefined || room === null){
-    return VmailNoValid();
-  }
-  else{    
-    return(
-      <React.Fragment>
-        
-        <Head>
-          <title>Page Email Validate</title>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
-  
-        <Mobile>
-            
-          <div className={s.container}>
-            <div className={s.icon}>
-              <ImageBlur src={"/pemilo.svg"} ></ImageBlur>
-            </div>
-            <div className={s.title}>
-              <p>kamu sudah masuk room</p>
-            </div>
-            <div className={s.instruction}>
-              <p className={s.medium}>{name}</p>
-              <p>di tahap ke <b> dua </b> kamu harus memasukan <b> email </b> yang kamu daftarkan kepada <b> panitia </b> sebelum <b> room </b> dimulai</p>
-            </div>
-            <div className={s.column}>
-  
-              <input onChange={(e)=>{setMail(e.target.value)}} type="email" name="mail" id="Rmail" className={s.input} placeholder="Email address"/>
-              
-              {(pw === true) ? <input onChange={(e)=>{setPass(e.target.value)}} type="password" name="pass" id="Rpass" className={s.input} placeholder="Password Room"/>  : null}
-  
-              <input onClick={()=>{atClick()}} type="button" value="lanjut" className={s.btn}/>
-  
-            </div>
+      <Mobile>
+          
+        <div className={s.container}>
+          <div className={s.icon}>
+            <ImageBlur src={"/pemilo.svg"} ></ImageBlur>
           </div>
-  
-        </Mobile>
-  
-      </React.Fragment>
-    )
-  }
+          <div className={s.title}>
+            <p>kamu sudah masuk room</p>
+          </div>
+          <div className={s.instruction}>
+            <p className={s.medium}>{name}</p>
+            <p>di tahap ke <b> dua </b> kamu harus memasukan <b> email </b> yang kamu daftarkan kepada <b> panitia </b> sebelum <b> room </b> dimulai</p>
+          </div>
+          <div className={s.column}>
 
+            <input onChange={(e)=>{setMail(e.target.value)}} type="email" name="mail" id="Rmail" className={s.input} placeholder="Email address"/>
+            
+            {(pw === true) ? <input onChange={(e)=>{setPass(e.target.value)}} type="password" name="pass" id="Rpass" className={s.input} placeholder="Password Room"/>  : null}
 
+            <input onClick={()=>{atClick()}} type="button" value="lanjut" className={s.btn}/>
+
+          </div>
+        </div>
+
+      </Mobile>
+
+    </React.Fragment>
+  )
     
   }
 
